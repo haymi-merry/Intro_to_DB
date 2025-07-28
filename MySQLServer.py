339732d65df9b0affc931657acd126dc48d7899f
haymi-merry <emutiyeee1996@gmail.com>
2025-07-28 19:20:46 +0300

    add mysqlsrver.py python script

---
+++ MySQLServer.py
@@ -0,0 +1,38 @@
+import mysql.connector
+from mysql.conncetor import Error
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def create_database():
+    try:
+
+        host= os.getenv("MYSQL_HOST")
+        port= os.getenv("MYSQL_PORt")
+        user= os.getenv("MYSQL_USER")
+        password= os.getenv("MYSQL_PASSWORD") 
+
+        connection = mysql.connector.connect(
+            host=host,
+            port=port,
+            user=user,
+            password=password
+        )
+
+        if connection.is_connceted():
+            cursor = connection.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXIXTS alx_book_store")
+            print("Database created successfully")
+        else:
+            print("Failed to connect to the database")
+    except Error as e:
+        print(f"Error While connceting to mSQL: {e}")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL conncetion is closed.")
+
+if __name__ == "_main_":
+    create_database()
